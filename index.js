746f4c9a6e7d4cd41ec2e87f2955dbac374d240b
4P0N <abidjahanapon@gmail.com>
2021-03-26 20:26:44 +0600

    completed

---
+++ index.js
@@ -0,0 +1,67 @@
+var drums = document.querySelectorAll(".drum");
+//if instead we wrote handleClick(), the function would trigger on page load without click
+
+for (var i = 0; i < drums.length; i++) {
+  drums[i].addEventListener("click", function () {
+    //alert(drums[i].innerHTML + " clicked"); -->this wont work as i will be always 7 here
+    var buttonName = this.textContent;
+    makeSound(buttonName);
+    animateButton(buttonName);
+  });
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    animateButton(event.key);
+});
+
+
+function makeSound(buttonName){
+    switch (buttonName) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+  
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+  
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+  
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+  
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+  
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+  
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        default:
+          console.log(buttonName);
+      }
+}
+
+function animateButton(buttonName){
+    var activeButton=document.querySelector("." + buttonName);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
